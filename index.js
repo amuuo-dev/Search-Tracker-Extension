f84fcd070635c6d51c9640710ef114f150dcb513
amuuo-dev <anthonmuuo92@gmail.com>
2024-04-29 15:11:51 +0300

    updation of index.js

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
-let myLeads= [];
+let myLeads = []
 const inputEl = document.getElementById("input-el")
-
 const inputBtn = document.getElementById("input-btn")
-
 const ulEl = document.getElementById("ul-el")
+const deleteBtn=document.getElementById("delete-btn")
+const tabBtn=document.getElementById("save-btn")
 
-const deleteBtn = document.getElementById("delete-btn")
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-const tabBtn = document.getElementById("save-btn")
-
-const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
-  
-if(leadsFromLocalStorage){
-    myLeads=leadsFromLocalStorage
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-tabBtn.addEventListener("click", function(){
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLead", JSON.stringify(myLeads))
-        render(myLeads)
-    })
-   
-})
+        tabBtn.addEventListener("click", function(){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                myLeads.push(tabs[0].url)
+                localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+                render(myLeads)
+            })
+            })
+         
 
-
-function render(leads){
+function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
-        <li>
-        <a target='_blank' href='${leads[i]}'>
+            <li>
+                <a target='_blank' href='${leads[i]}'>
                     ${leads[i]}
                 </a>
-        </li>`
+            </li>
+        `
     }
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems  
 }
-
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()
@@ -47,9 +42,11 @@ deleteBtn.addEventListener("dblclick", function(){
     render(myLeads)
 })
 
+
 inputBtn.addEventListener("click", function() {
-   myLeads.push(inputEl.value)
-  inputEl.value=""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-  render(myLeads)
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
+
